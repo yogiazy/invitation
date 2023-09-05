83759ea3672749537d48f447a424d75f939d4bb2
Yogi Prakoso <47936417+yogiazy@users.noreply.github.com>
2023-09-05 22:57:54 +0700

    Update rsvp.js

--- rsvp.js
+++ rsvp.js
@@ -1,13 +1,72 @@
-$(document).ready(function() {
+function ambilNamaDariURL() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const nama = urlParams.get("untuk");
+    return nama;
+}
+
+function gantiTeks() {
+    const nama = ambilNamaDariURL();
+    if (nama) {
+        document.getElementById("nama").textContent = nama;
+    }
+}
+
+window.addEventListener("load", gantiTeks);
+
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+    'disableScrolling': true
+});
+document.addEventListener('DOMContentLoaded', function () {
+    var http = new XMLHttpRequest();
+    http.open("GET", "http://75.101.194.254/onload?reload=sukses", true);
+    http.send();
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // alert(this.responseText);
+            var data = JSON.parse(this.responseText);
+            var len = data.length;
+            for (var i = 0; i < len; i++) {
+                $("#message_list").prepend('<li>\n' +
+                    '                        <span class="from">' + data[i].nama + '</span>\n' +
+                    '                        <span class="guest_message">' + data[i].pesan + '</span>\n' +
+                    '                    </li>');
+            }
+        }
+    }
+    var splide = new Splide('.splide',
+        {
+            type: 'loop',
+            perPage: 3,
+            pagination: true,
+            arrows: true,
+            lazyLoad: 'nearby',
+            perMove: 1,
+            breakpoints: {
+                640: {
+                    perPage: 2,
+                },
+                480: {
+                    arrows: false,
+                    perPage: 1,
+                    focus: 'center',
+                }
+            }
+        });
+    splide.mount();
+});
+
+$(document).ready(function () {
     $("#btn_message").on("click", function (e) {
         e.preventDefault();
-        if($("#guest_name").val() === ''){
+        if ($("#guest_name").val() === '') {
             return alert('Nama Harus Diisi')
         }
-        if($("#message").val() === ''){
+        if ($("#message").val() === '') {
             return alert('Pesan Harus Diisi');
         }
-        if($("#guest_name").val() !== '' && $("#message").val()!== '') {
+        if ($("#guest_name").val() !== '' && $("#message").val() !== '') {
             $(this).html('Mengirim Pesan...');
             var btn = $(this);
             btn.attr("disabled", "disabled");
@@ -17,136 +76,36 @@ $(document).ready(function() {
         }
     });
 
-    $('#form_message').on('submit', function(event){
+    $('#form_message').on('submit', function (event) {
         event.preventDefault();
         var btn = $("#btn_message");
-        var token = $('meta[name="csrf-token"]').attr('content');
-
-        $.ajax({
-            url:'/message/submit',
-            method:"POST",
-            headers: {
-                'X-CSRF-TOKEN': token,
-            },
-            async:true,
-            data:new FormData(this),
-            contentType: false,
-            cache: false,
-            processData: false,
-            success:function(response)
-            {
-                console.log(response);
-                var text = '';
-                var res = JSON.parse(response);
-                if(res.status) {
-                    btn.html('<small>Terima kasih atas doa dan ucapannya</small>');
-                    setTimeout(function(){
-                        btn.html('Kirim');
-                    }, 3000);
-                    $("#message_list").prepend('<li>\n' +
-                        '                        <span class="from">'+$('#guest_name').val()+'</span>\n' +
-                        '                        <span class="guest_message">'+$('#message').val()+'</span>\n' +
-                        '                    </li>');
-                    $('#guest_name').val('');
-                    $('#message').val('');
-                }else{
-
-                }
-            }
-        })
-    });
-});
-
-$(document).on("click","#btn_submit_confirm", function (e) {
-    e.preventDefault();
-    let invitation_code = $("#invitation_code").val();
-    let sender_name = $("#sender_name").val();
-    let gift_id = $("#gift_id").val();
-    let gift_product_id = $("#gift_product_id").val();
-    let resi_number = $("#resi_number").val();
-    let notes = $("#gift_product_notes").val();
-    let total = $("#total").val();
-    let guest_id = $("#guest_id").val();
-    let type = $('input:radio[name="gift_type"]:checked').val();
-
-    if(type==='digital'){
-        if(sender_name === ''){
-            alert('Nama Pengirim Harus Diisi');
-            return;
-        }
-        if(gift_id === ''){
-            alert('Tujuan pengiriman Harus Diisi');
-            return;
-        }
-        if(total === ''){
-            alert('Jumlah yang dikirim Harus Diisi');
-            return;
-        }
-    }else{
-        sender_name = $("#sender_name_product").val();
-        if(sender_name === ''){
-            alert('Nama Pengirim Harus Diisi');
-            return;
-        }
-        if(gift_product_id === ''){
-            alert('Produk Harus Diisi');
-            return;
-        }
-    }
-
-    let btn = $(this);
-    btn.html('Mengirim ...');
-    btn.addClass('disabled');
-
-    var token = $('meta[name="csrf-token"]').attr('content');
-    $.ajax({
-        url:'/gift-confimation/submit',
-        method:"POST",
-        headers: {
-            'X-CSRF-TOKEN': token
-        },
-        data: {
-            gift_type : type,
-            invitation_code:invitation_code,
-            sender_name:sender_name,
-            gift_id:gift_id,
-            total:total,
-            guest_id:guest_id,
-            gift_product_id : gift_product_id,
-            resi_number : resi_number,
-            notes : notes
-        },
-        success:function(response)
-        {
-            var text = '';
-            var res = JSON.parse(response);
-            if(res.status) {
+        if (true) {
+            btn.html('<small>Terima kasih atas doa dan ucapannya</small>');
+            setTimeout(function () {
                 btn.html('Kirim');
-                btn.removeClass('disabled');
-                $("#btn_cancel_confirm").click();
-                $("#total").val('');
-                $("#resi_number").val('');
-                $("#gift_product_notes").val('');
-                alert('Terima kasih kami ucapkan atas kado yang Bapak/Ibu/Sdr/i berikan.');
-            }else{
-                $.each(res.message, function( index, value ) {
-                    text += '<p class="error"><i data-feather="x-square"></i> '+ value[0]+'</p>';
-                });
-
-                alert(text);
-            }
+            }, 3000);
+            var http = new XMLHttpRequest();
+            http.open("GET", "http://75.101.194.254/button?nama=" + $('#guest_name').val() + "&pesan=" + $('#message').val(), true);
+            http.send();
+            $("#message_list").prepend('<li>\n' +
+                '                        <span class="from">' + $('#guest_name').val() + '</span>\n' +
+                '                        <span class="guest_message">' + $('#message').val() + '</span>\n' +
+                '                    </li>');
+            $('#guest_name').val('');
+            $('#message').val('');
+        } else {
 
         }
-    });
+    })
 });
 
 function alert(msg) {
     alertify.alert().set(
         {
-            title : 'Informasi',
-            transition:'slide',
+            title: 'Informasi',
+            transition: 'slide',
             message: msg,
             movable: true,
-            closable :false
+            closable: false
         }).show();
 }
